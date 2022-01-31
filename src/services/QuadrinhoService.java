594b293eaa585228481d0845e55dee548c56ebcd
Higor Rafael Moura RabÊlo <86304823+higorrabelo@users.noreply.github.com>
2022-01-31 10:43:29 -0300

    Prototype Gui implemented with Comics and Comics Service Classes Implemented

---
+++ src/services/QuadrinhoService.java
@@ -0,0 +1,34 @@
+package services;
+import db.*;
+import model.Quadrinho;
+import java.sql.*;
+import javax.swing.JOptionPane;
+public class QuadrinhoService {
+    
+    private Quadrinho quad = null;
+    private Connection conn = null;
+    
+    public void insereQuadrinho(Quadrinho qd){
+        try{
+            conn = DB.getConnection();
+            PreparedStatement st = conn.prepareStatement(
+                    "insert into quadrinhos(nome_quadrinho, nome_editora, numero, mes, ano) "
+                    +"values "
+                    +"(?,?,?,?,?)");
+            st.setString(1,qd.getNome_quadrinho());
+            st.setString(2,qd.getNome_editora());
+            st.setInt(3, qd.getNumero());
+            st.setString(4,qd.getMes());
+            st.setInt(5, qd.getAno());  
+            int rows = st.executeUpdate();
+            
+            JOptionPane.showMessageDialog(null,"Quadrinho Inserido com Sucesso");
+            
+        }
+        catch(SQLException e){
+            throw new DbException("Erro ao inserir Quadrinho no banco"+e.getMessage());
+        }
+        
+    }
+    
+}
