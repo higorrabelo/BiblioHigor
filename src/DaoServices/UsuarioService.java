471a8f8c959f94cc39d0d639bcf9f0821052a7d1
Higor Rafael Moura RabÊlo <86304823+higorrabelo@users.noreply.github.com>
2022-02-04 17:49:50 -0300

    Gui for user implementation started

--- src/DaoServices/UsuarioService.java
+++ src/DaoServices/UsuarioService.java
@@ -1,8 +1,12 @@
 package DaoServices;
 import java.sql.Connection;
 import dao.UsuarioDAO;
+import db.DbException;
 import java.util.List;
 import model.Usuario;
+import java.sql.*;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
 
 public class UsuarioService implements UsuarioDAO{
 
@@ -16,27 +20,131 @@ public class UsuarioService implements UsuarioDAO{
     
     @Override
     public void insere(Usuario obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "insert into usuario(nome_usuario,senha_usuario,email_usuario,nascimento,cadastro,endereco) values ( ? , md5(?), ? , ? , ? , ?)";
+        PreparedStatement st = null;
+        try{
+            st = conn.prepareStatement(sql);
+            st.setString(1,obj.getNome_usuario());
+            st.setString(2,obj.getSenha_usuario());
+            st.setString(3,obj.getEmail_usuario());
+            st.setDate(4,new java.sql.Date(obj.getNascimento().getTime()));
+            st.setDate(5,new java.sql.Date(obj.getCadastro().getTime()));
+            st.setString(6,obj.getEndereco());
+            int rows = st.executeUpdate();
+            
+            if(rows > 0){
+                JOptionPane.showMessageDialog(null,"Usuario Inserido com Sucesso");
+            }else{
+                JOptionPane.showMessageDialog(null,"Falha ao Inserir o Usuário");
+            }
+        }
+        catch(SQLException e){
+            throw new DbException("Erro na Inserção dos dados de Usuário em banco");
+        }
     }
 
     @Override
     public void update(Usuario obj) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "update usuario set nome_usuario = ?, senha_usuario = md5(?) , email_usuario = ? ,"
+                    + " nascimento = ? , cadastro = ? , endereco = ?  where id_usuario = ? ";
+        PreparedStatement st = null;
+        try{
+            st = conn.prepareStatement(sql);
+            st.setString(1, obj.getNome_usuario());
+            st.setString(2, obj.getSenha_usuario());
+            st.setString(3, obj.getEmail_usuario());
+            st.setDate(4, new java.sql.Date(obj.getNascimento().getTime()));
+            st.setDate(5, new java.sql.Date(obj.getCadastro().getTime()));
+            st.setString(6, obj.getEndereco());
+            
+            int rows = st.executeUpdate();
+            
+            if(rows > 0){
+                JOptionPane.showMessageDialog(null,"Usuário Atualizado com Sucesso");
+            }else{
+                JOptionPane.showMessageDialog(null,"Falha na Atualização do usuário");
+            }
+            
+        
+        }
+        catch(SQLException e){
+            throw new DbException("Erro ao Atualizar dados do Usuário");
+        }
     }
 
     @Override
     public void delete(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         String sql = "delete from usuario where id_usuario = ? ";
+            PreparedStatement st = null;
+            try{
+                st = conn.prepareStatement(sql);
+                st.setInt(1, id);
+                int row = st.executeUpdate();
+                if(row>0){
+                    JOptionPane.showMessageDialog(null, "Ususário Apagado com Sucesso");
+                }else{
+                    JOptionPane.showMessageDialog(null, "Falha ao Remover Usuário: Inexistente");
+                }   
+            }
+            catch(SQLException e){
+                throw new DbException("Erro ao Remover Usuários");
+            }
     }
 
     @Override
     public Usuario acharPorId(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            String sql = "select * from usuario where id_usuario = ? ";
+            PreparedStatement st = null;
+            try{
+                st = conn.prepareStatement(sql);
+                st.setInt(1, id);
+                ResultSet rs = st.executeQuery();
+                Usuario user = null;
+
+                while(rs.next()){
+                    user = new Usuario(
+                                       rs.getInt("id_usuario"),
+                                       rs.getString("nome_usuario"),
+                                       rs.getString("senha_usuario"),
+                                       rs.getString("email_usuario"),
+                                       rs.getDate("nascimento"),
+                                       rs.getDate("cadastro"),
+                                       rs.getString("endereco")
+                                       );
+                }
+                return user;
+            }
+            catch(SQLException e){
+                throw new DbException("Erro ao Listar Usuários");
+            }
     }
 
     @Override
     public List<Usuario> listarUsuario() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "select * from usuario";
+        PreparedStatement st = null;
+        try{
+            st = conn.prepareStatement(sql);
+            ResultSet rs = st.executeQuery();
+            List<Usuario> list = new ArrayList<>();
+            
+            while(rs.next()){
+                Usuario user = new Usuario(
+                                           rs.getInt("id_usuario"),
+                                           rs.getString("nome_usuario"),
+                                           rs.getString("senha_usuario"),
+                                           rs.getString("email_usuario"),
+                                           rs.getDate("nascimento"),
+                                           rs.getDate("cadastro"),
+                                           rs.getString("endereco")
+                                           );
+                list.add(user);
+            }
+            return list;
+        }
+        catch(SQLException e){
+            throw new DbException("Erro ao Listar Usuários");
+        }
     }
     
 }
--- src/application/Program.java
+++ src/application/Program.java
@@ -5,14 +5,39 @@
  */
 package application;
 
+import dao.UsuarioDAO;
+import dao.DaoFactory;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import model.Usuario;
+import java.util.Date;
+
 /**
  *
  * @author higor_x
  */
 public class Program {
     
-    public static void main(String[] args){
-    
+    public static void main(String[] args) throws ParseException{
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        UsuarioDAO userDao = DaoFactory.criarUsuario();
+        
+        //Usuario user = new Usuario("Edmilson","Edmilson","edmilson@gmail.com",sdf.parse("24/09/1954"),new Date(),"Avenida Duque de Caxias");
+        //userDao.insere(user);
+        
+        userDao.delete(3);
+       /*
+        
+         for(Usuario user : userDao.listarUsuario()){
+            System.out.println(user);
+        }
+        
+        System.out.println(userDao.acharPorId(2));
+        
+        */ 
+       
+        
+        
     }
     
 }
